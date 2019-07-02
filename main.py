ca5f45842af4448f31ef1a5234598532e00489dd
Karan <adwitiya.singh.579@my.csun.edu>
2019-07-02 13:50:26 -0700

    added search in json

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import json
 
+
+def getpath(nested_dict, key):
+    for k, v in nested_dict.items():
+        if k == key:
+            return v
+        elif hasattr(v, 'items'):
+            p = getpath(v, key)
+            if p is not None:
+                return p
+
+
 # read file
 with open('sample_request.json', 'r') as myfile:
     data=myfile.read()
@@ -7,24 +18,11 @@ with open('sample_request.json', 'r') as myfile:
 # parse file
 obj = json.loads(data)
 
-print("name: " + str(obj['username']))
 
 id = 1
 data = json.loads(open('sample_template.json').read())
 
-
-
-data = {}
-data['id'] = id
-id+=1
-data['tasks'] = str(obj['title'])
-data['notes'] = str(obj['notes'])
-data['owner'] = []
-data['owner'].append({
-     'username': str(obj['username']),
-     'fullname': str(obj['name']['first']+" "+obj['name']['last'])
- })
-data['completed'] = bool(obj['completed'])
+print(getpath(obj,'last'))
 
 with open('response.json', 'w') as outfile:
         json.dump(data,outfile, indent='\t')
