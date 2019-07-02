c59d0bc070c64729a3bd2744e206336aba08d525
Karan <adwitiya.singh.579@my.csun.edu>
2019-07-02 16:03:25 -0700

    fully functional

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
 import json
 
 
-def getpath(nested_dict, key):
-    for k, v in nested_dict.items():
-        if k == key:
-            return v
-        elif hasattr(v, 'items'):
-            p = getpath(v, key)
-            if p is not None:
-                return p
+def getval(nested_dict: dict, key: str) -> str:
+    vals = key.split()
+    x = ""
+    for val in vals:
+        val.replace(" ", "")
+        if(val[0]=='{'):
+            val = val[1:len(val) - 1]
+        for k, v in nested_dict.items():
+            if k == val:
+                x += v
+            elif isinstance(v, dict):
+                x += getval(v, val) + " "
+
+    return x
+
+
+def poptemp(template: dict, request_dict: dict):
+    for k, v in template.items():
+        if hasattr(v, 'items'):
+            poptemp(v, request_dict)
+        else:
+            if isinstance(v, str):
+                template[k] = getval(request_dict, v)
 
 
 # read file
 with open('sample_request.json', 'r') as myfile:
-    data=myfile.read()
+    data = myfile.read()
 
 # parse file
 obj = json.loads(data)
 
-
-id = 1
 data = json.loads(open('sample_template.json').read())
 
-print(getpath(obj,'last'))
+poptemp(data, obj)
+
+# print(data)
+
 
 with open('response.json', 'w') as outfile:
-        json.dump(data,outfile, indent='\t')
+    json.dump(data, outfile, indent='\t')
