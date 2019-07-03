4d6b2fa28c41474b13de436133d4b03281d71161
Karan <adwitiya.singh.579@my.csun.edu>
2019-07-03 14:31:35 -0700

    added database

--- method2.py
+++ method2.py
@@ -26,7 +26,7 @@ def get_cursor(name: str = "tasks.db"):
 
 
 def create_table():
-    sql_command = ("CREATE TABLE tasks (  \n"
+    sql_command: str = ("CREATE TABLE tasks (  \n"
                    "    id INTEGER PRIMARY KEY,  \n"
                    "    task VARCHAR(50),  \n"
                    "    notes VARCHAR(100),  \n"
@@ -39,35 +39,42 @@ def create_table():
 
 
 def insert(values: dict):
-    flat_values = {}
+    flat_values: dict = {}
     flatten_dict(values, flat_values)
     if flat_values['completed']:
         flat_values['completed'] = 1
     else:
         flat_values['completed'] = 0
-    sql_command = """INSERT INTO tasks VALUES ("{id}", "{task}", "{notes}", "{username}", "{fullname}", 
+    sql_command: str = """INSERT INTO tasks VALUES ("{id}", "{task}", "{notes}", "{username}", "{fullname}", 
     "{completed}", "{subtasks}");""".format(**flat_values)
     cursor = get_cursor()
     cursor.execute(sql_command)
 
 
 def delete(id: int):
-    sql_command = "DELETE from Student where id='{id}'".format(id)
+    sql_command: str = "DELETE from Student where id='{id}'".format(id)
     cursor = get_cursor()
     cursor.execute(sql_command)
 
+def mark_complete(id: int):
+    sql_command = "UPDATE tasks SET completed = 1 where id='{id}'".format(id)
+    cursor = get_cursor()
+    cursor.execute(sql_command)
 
+    
 def showall():
+    sql_command: str = "SELECT * FROM tasks"
     cursor = get_cursor();
-    cursor.execute("SELECT * FROM tasks")
+    cursor.execute(sql_command)
     all = cursor.fetchall()
     for i in all:
         print(i)
 
 
 def showall_incomp():
-    cursor = get_cursor();
-    cursor.execute("SELECT * FROM tasks WHERE completed = 0")
+    sql_command: str = "SELECT * FROM tasks WHERE completed = 0"
+    cursor = get_cursor(sql_command);
+    cursor.execute()
     all = cursor.fetchall()
     for i in all:
         print(i)
