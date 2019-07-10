fc1b9397112fa2d0847eb421021d843c3b769772
Karan <adwitiya.singh.579@my.csun.edu>
2019-07-10 13:47:18 -0700

    cleaned up fire structure

--- database.py
+++ database.py
@@ -1,64 +1,82 @@
 import sqlite3
-from main import *
+from jsonload import *
 
 
-def get_cursor(name: str = "tasks.db"):
-    connection = sqlite3.connect(name)
-    cursor = connection.cursor()
-    return cursor
+
 
 
 def create_table():
-    sql_command: str = ("CREATE TABLE tasks (  \n"
+    connection = sqlite3.connect("myDB.db")
+    cursor = connection.cursor()
+    sql_command: str = ("CREATE TABLE IF NOT EXISTS tasks (  \n"
                    "    id INTEGER PRIMARY KEY,  \n"
                    "    task VARCHAR(50),  \n"
                    "    notes VARCHAR(100),  \n"
                    "    username VARCHAR(30),\n"
                    "    fullname VARCHAR(30),\n"
                    "    completed INTEGER,\n"
-                   "    subtasks INTEGER;")
-    cursor = get_cursor()
+                   "    subtasks INTEGER)")
     cursor.execute(sql_command)
-
+    connection.commit()
+    connection.close()
 
 def insert(values: dict):
+    connection = sqlite3.connect("myDB.db")
+    cursor = connection.cursor()
     flat_values: dict = {}
     flatten_dict(values, flat_values)
     if flat_values['completed']:
         flat_values['completed'] = 1
     else:
         flat_values['completed'] = 0
-    sql_command: str = """INSERT INTO tasks VALUES ("{id}", "{task}", "{notes}", "{username}", "{fullname}", 
-    "{completed}", "{subtasks}");""".format(**flat_values)
-    cursor = get_cursor()
+    if flat_values['subtasks'] is None:
+        flat_values['subtasks'] = 0
+    sql_command: str = """REPLACE  INTO tasks VALUES ("{id}", "{task}", "{notes}", "{username}", "{fullname}", 
+    "{completed}", "{subtasks}")""".format(**flat_values)
     cursor.execute(sql_command)
+    connection.commit()
+    connection.close()
 
 
 def delete(id: int):
-    sql_command: str = "DELETE from Student where id='{id}'".format(id)
-    cursor = get_cursor()
+    connection = sqlite3.connect("myDB.db")
+    cursor = connection.cursor()
+    sql_command: str = "DELETE from tasks where id='{}'".format(id)
     cursor.execute(sql_command)
+    connection.commit()
+    connection.close()
+
 
 def mark_complete(id: int):
-    sql_command = "UPDATE tasks SET completed = 1 where id='{id}'".format(id)
-    cursor = get_cursor()
+    connection = sqlite3.connect("myDB.db")
+    cursor = connection.cursor()
+    sql_command = "UPDATE tasks SET completed = 1 where id='{}'".format(id)
     cursor.execute(sql_command)
+    connection.commit()
+    connection.close()
 
 
 def showall():
+    connection = sqlite3.connect("myDB.db")
+    cursor = connection.cursor()
     sql_command: str = "SELECT * FROM tasks"
-    cursor = get_cursor();
     cursor.execute(sql_command)
     all = cursor.fetchall()
     for i in all:
         print(i)
+    connection.commit()
+    connection.close()
 
 
 def showall_incomp():
+    connection = sqlite3.connect("myDB.db")
+    cursor = connection.cursor()
     sql_command: str = "SELECT * FROM tasks WHERE completed = 0"
-    cursor = get_cursor(sql_command);
-    cursor.execute()
+    cursor.execute(sql_command)
     all = cursor.fetchall()
     for i in all:
         print(i)
+    connection.commit()
+    connection.close()
+
 
